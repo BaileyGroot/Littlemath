b688599285717845458c3569dc70943d3d99138e
Grootrix <grootrix@gmail.com>
2021-08-09 13:03:42 +0200

    Add project files.

---
+++ Littlemath/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Littlemath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lidt Matematik
+
+
+
+            byte a = 1;
+
+            byte b = 2;
+
+
+
+            Console.WriteLine(a + b);
+
+            Console.WriteLine(a / b);
+
+            double test1 = Math.Pow(a, 2);
+            double test2 = Math.Pow(b, 2);
+            
+            Console.WriteLine(test1 + " og " + test2);
+
+
+            // Prevents the program from closing.
+            Console.ReadKey();
+        }
+    }
+}
